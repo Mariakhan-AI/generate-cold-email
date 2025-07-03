3e4e355c5840d67310918e77472ea1de5977e767
Mariakhan-AI <mk5266157@gmail.com>
2025-07-03 05:21:39 -0700

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -6,7 +6,7 @@ import uuid
 class Portfolio:
     def __init__(self, file_path="C:/Users/Administrator/Documents/cold email/portfolio.csv"):
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.chroma_client = chromadb.Client()
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
